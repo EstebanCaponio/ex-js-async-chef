71b65aeb1c7297335a1bccabed092a557cc07822
EstebanCaponio <estebancaponio@gmail.com>
2025-06-30 18:03:00 +0200

    feat: bonus 1

---
+++ snack1 - bonus1/script.js
@@ -0,0 +1,44 @@
+async function getChefBirthday(id) {
+
+    let data;
+
+    try {
+        const responseRecipes = await fetch(`https://dummyjson.com/recipes/${id}`);
+
+        data = await responseRecipes.json()
+
+    } catch (error) {
+        console.error(error);
+    }
+    if (data.message) {
+        throw new Error("id inesistente");
+    }
+
+    let birthday;
+    try {
+        const responseChef = await fetch(`https://dummyjson.com/users/${data.userId}`);
+
+        const responseJson = await responseChef.json()
+
+        birthday = await responseJson.birthDate;
+
+        return birthday
+
+    } catch (error) {
+        console.error(error);
+    }
+    if (birthday.message) {
+        throw new Error("chef non trovato");
+    }
+
+}
+
+(async () => {
+    try {
+        const chefBirth = await getChefBirthday(1);
+        console.log(`Data di nascita dello chef: ${chefBirth}`);
+    } catch (error) {
+        console.error(error.message);
+    }
+    console.log('programma finito!')
+})();
